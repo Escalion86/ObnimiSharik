9c8e7ba8dee175b8a7770a135647b1f2b9f8b790
Aleksei Belinskiy <escalion86@gmail.com>
2021-07-15 22:25:53 +0700

    Added constant DEFAULT_PRODUCT

---
+++ helpers/constants.js
@@ -0,0 +1,14 @@
+export const DEFAULT_USER = {
+  name: 'Гость',
+  email: '',
+  image: null,
+  role: 'client',
+}
+
+export const DEFAULT_PRODUCT = {
+  name: '',
+  description: '',
+  price: 0,
+  image_url: '',
+  types: [],
+}
