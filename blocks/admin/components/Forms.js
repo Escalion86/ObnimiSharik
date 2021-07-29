fd25c9cb5abe5ac31e119180f712f0e51c02fbaf
Aleksei Belinskiy <escalion86@gmail.com>
2021-07-29 15:36:44 +0700

    Added cloudinary & Updated Forms

--- blocks/admin/components/Forms.js
+++ blocks/admin/components/Forms.js
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 // import { useRouter } from 'next/router'
 import { mutate } from 'swr'
 
 import Button from '@components/Button'
+
+import ImageZoom from 'react-medium-image-zoom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import {
   DEFAULT_PRODUCT,
@@ -12,6 +17,7 @@ import {
 } from '@helpers/constants'
 
 import MultiselectCheckbox from '@admincomponents/MultiselectCheckbox'
+import { resolveMotionValue } from 'framer-motion'
 
 const Form = ({
   handleSubmit = () => {},
@@ -43,6 +49,16 @@ const Form = ({
   )
 }
 
+const InputComponent = (props) => {
+  const newProps = { ...props }
+  delete newProps['textarea']
+  return props.textarea ? (
+    <textarea {...newProps} rows={4} />
+  ) : (
+    <input {...newProps} />
+  )
+}
+
 const Input = ({
   label = '',
   type,
@@ -50,23 +66,178 @@ const Input = ({
   name,
   value,
   onChange,
-  required,
-}) => (
-  <div className="flex flex-col">
-    <label htmlFor={name}>{label}</label>
-    <input
-      className="px-2 py-1 bg-gray-200 border border-gray-700 rounded-lg"
-      type={type}
-      maxLength={maxLength}
-      name={name}
-      value={value}
-      onChange={onChange}
-      required={required}
-    />
-  </div>
-)
+  required = false,
+  textarea,
+  accept,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={name}>{label}</label>
+      <InputComponent
+        className="px-2 py-1 bg-gray-200 border border-gray-700 rounded-lg"
+        type={type}
+        maxLength={maxLength}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        textarea={textarea}
+        accept={accept}
+      />
+    </div>
+  )
+}
+
+const PriceInput = ({ value, onChange, required = false }) => {
+  if (!parseInt(value)) value = 0
+  return (
+    <div className="flex flex-col w-32">
+      <label htmlFor="price">Стоимость</label>
+      <div className="flex w-full border border-gray-700 rounded-lg flex-nowrap">
+        <input
+          className="flex-1 w-24 px-2 py-1 bg-gray-200 rounded-l-lg"
+          type="text"
+          name="price"
+          value={parseInt(value)}
+          onChange={onChange}
+          required={required}
+          onKeyPress={(e) => {
+            e = e || window.event
+            var charCode = typeof e.which == 'undefined' ? e.keyCode : e.which
+            if (!(charCode >= 48 && charCode <= 57)) {
+              e.preventDefault()
+            }
+          }}
+        />
+        <div className="flex items-center justify-center w-6 bg-gray-300 border-l border-gray-700 rounded-r-lg">
+          ₽
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const InputImages = ({
+  image_urls = [],
+  onChange = () => {},
+  onAddImage = () => {},
+}) => {
+  const hiddenFileInput = useRef(null)
+  const addImageClick = (event) => {
+    hiddenFileInput.current.click()
+  }
+
+  const handleChange = (e) => {
+    onAddImage(e.target.files[0])
+  }
+
+  return (
+    <div className="flex flex-col">
+      <label>Картинки</label>
+      <div className="flex flex-wrap w-full gap-2 px-1.5 py-1 bg-gray-200 border border-gray-700 rounded-lg">
+        {image_urls.map((image_url, index) => (
+          <div key={index} className="relative">
+            <ImageZoom
+              image={{
+                src: image_url,
+                alt: 'special',
+                className: 'w-20 h-20',
+                // style: { width: '50em' }
+              }}
+              zoomImage={{
+                src: image_url,
+                alt: 'product_image',
+              }}
+            />
+            <FontAwesomeIcon
+              className="absolute top-0 right-0 text-red-700 duration-200 transform cursor-pointer hover:scale-125"
+              icon={faTrash}
+              size="1x"
+              onClick={() => {
+                // image_urls.splice(index, 1)
+                onChange(image_urls.filter((image_url, i) => i !== index))
+              }}
+            />
+          </div>
+        ))}
+        {image_urls.length < 4 ? (
+          <div
+            onClick={addImageClick}
+            className="flex items-center justify-center w-20 h-20 bg-white border-2 border-gray-500 cursor-pointer rounded-xl"
+          >
+            <div className="flex items-center justify-center w-20 transparent hover:scale-150 ">
+              <FontAwesomeIcon
+                className="text-gray-700"
+                icon={faPlus}
+                size="2x"
+                // onClick={() => {
+                //   image_urls.splice(index, 1)
+                //   onChange(image_urls)
+                // }}
+              />
+              <input
+                type="file"
+                ref={hiddenFileInput}
+                onChange={handleChange}
+                style={{ display: 'none' }}
+                accept="image/jpeg,image/png"
+              />
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
 
 const contentType = 'application/json'
+
+const compareArrays = (arrayOld, arrayNew) => {
+  if (arrayOld.length === 0) return []
+  return arrayOld.filter((data) => !arrayNew.includes(data))
+}
+
+const deleteImages = async (arrayOfImagesUrls) => {
+  if (arrayOfImagesUrls.length > 0)
+    await Promise.all(
+      arrayOfImagesUrls.map(async (imageUrl) => {
+        if (imageUrl.lastIndexOf('obnimisharik/') > 0)
+          await deleteImage(
+            imageUrl.substring(
+              imageUrl.lastIndexOf('obnimisharik/'),
+              imageUrl.lastIndexOf('.')
+            )
+          )
+      })
+    )
+}
+
+const deleteImage = async (imagePublicId) => {
+  // const { id } = router.query
+
+  try {
+    const res = await fetch('/api/cloudimages', {
+      method: 'DELETE',
+      // headers: {
+      //   Accept: contentType,
+      //   'Content-Type': contentType,
+      // },
+      body: imagePublicId,
+    })
+
+    // Throw error with status code in case Fetch API req failed
+    if (!res.ok) {
+      throw new Error(res.status)
+    }
+
+    // const { data } = await res.json()
+    // console.log(`data`, data)
+    // mutate(url, data, false) // Update the local data without a revalidation
+    // afterConfirm()
+  } catch (error) {
+    // setMessage('Failed to update on ' + url)
+  }
+}
 
 const putData = async (url, form, afterConfirm, setMessage) => {
   // const { id } = router.query
@@ -111,10 +282,38 @@ const postData = async (url, form, afterConfirm, setMessage) => {
     if (!res.ok) {
       throw new Error(res.status)
     }
-
+    // const { data } = await res.json()
+    // mutate(url, data, false)
     afterConfirm()
   } catch (error) {
     setMessage('Failed to add on ' + url)
+  }
+}
+
+const sendImage = async (image, form, setForm) => {
+  if (typeof image === 'object') {
+    const formData = new FormData()
+    formData.append('file', image)
+    formData.append('upload_preset', 'obnimisharik')
+
+    const image_url = await fetch(
+      'https://api.cloudinary.com/v1_1/escalion-ru/image/upload',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.secure_url !== '') {
+          return data.secure_url
+        }
+      })
+      .catch((err) => console.error(err))
+    setForm({
+      ...form,
+      image_urls: [...form.image_urls, image_url],
+    })
   }
 }
 
@@ -136,36 +335,77 @@ export const ProductForm = ({
     archive: product.archive,
   })
 
+  const afterConfirmUpd = (data) => {
+    deleteImages(compareArrays(product.image_urls, form.image_urls))
+    afterConfirm(data)
+  }
+
   const forNew = product._id === undefined
 
   const handleChange = (e) => {
     const target = e.target
-    const value =
-      target.name === 'price'
-        ? target.value * 100
-        : target.name === 'image_urls'
-        ? [target.value]
-        : target.value
     const name = target.name
-
+    // deleteImage('obnimisharik/cmurzvh8hj7e7pcwefvx')
+    // if (name === 'image') {
+    //   setImages([...images, target.files[0]])
+    // } else {
+    const value =
+      name === 'price'
+        ? target.value * 100
+        : // : name === 'image_urls'
+          // ? [target.value]
+          target.value
     setForm({
       ...form,
       [name]: value,
     })
+    // }
+  }
+
+  // const sendImages = async (images) => {
+  //   const image_urls = await Promise.all(
+  //     images.map(async (file) => {
+  //       const formData = new FormData()
+  //       formData.append('file', file)
+  //       formData.append('upload_preset', 'obnimisharik')
+
+  //       const res = await fetch(
+  //         'https://api.cloudinary.com/v1_1/escalion-ru/image/upload',
+  //         {
+  //           method: 'POST',
+  //           body: formData,
+  //         }
+  //       )
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //           if (data.secure_url !== '') {
+  //             return data.secure_url
+  //           }
+  //         })
+  //         .catch((err) => console.error(err))
+  //       return res
+  //     })
+  //   )
+
+  //   return image_urls
+  // }
+
+  const sendForm = async () => {
+    forNew
+      ? postData('/api/products', form, afterConfirmUpd, setMessage)
+      : putData(
+          `/api/products/${product._id}`,
+          form,
+          afterConfirmUpd,
+          setMessage
+        )
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const errs = formValidate()
     if (Object.keys(errs).length === 0) {
-      forNew
-        ? postData('/api/products', form, afterConfirm, setMessage)
-        : putData(
-            `/api/products/${product._id}`,
-            form,
-            afterConfirm,
-            setMessage
-          )
+      sendForm()
     } else {
       setErrors({ errs })
     }
@@ -206,16 +446,10 @@ export const ProductForm = ({
         value={form.description}
         onChange={handleChange}
         required
+        textarea
       />
-      <Input
-        key="price"
-        label="Стоимость"
-        type="number"
-        name="price"
-        value={form.price / 100}
-        onChange={handleChange}
-      />
-      <Input
+      <PriceInput value={form.price / 100} onChange={handleChange} />
+      {/* <Input
         key="image_urls"
         label="Ссылка на картинку"
         type="url"
@@ -223,7 +457,7 @@ export const ProductForm = ({
         value={form.image_urls[0]}
         onChange={handleChange}
         required
-      />
+      /> */}
       <MultiselectCheckbox
         title="Типы"
         options={productTypes.map((type) => {
@@ -240,6 +474,26 @@ export const ProductForm = ({
           })
         }}
       />
+      <InputImages
+        image_urls={form.image_urls}
+        onChange={(image_urls) =>
+          setForm({
+            ...form,
+            image_urls,
+          })
+        }
+        onAddImage={(image) => sendImage(image, form, setForm)}
+      />
+      {/* <Input
+        key="image"
+        label="Картинка"
+        type="file"
+        name="image"
+        // value={form.image}
+        onChange={handleChange}
+        accept="image/jpeg,image/png"
+        required
+      /> */}
     </Form>
   )
 }
@@ -262,6 +516,11 @@ export const SetForm = ({
     products_id: set.products_id,
     archive: set.archive,
   })
+
+  const afterConfirmUpd = (data) => {
+    deleteImages(compareArrays(set.image_urls, form.image_urls))
+    afterConfirm(data)
+  }
 
   const forNew = set._id === undefined
 
@@ -286,8 +545,8 @@ export const SetForm = ({
     const errs = formValidate()
     if (Object.keys(errs).length === 0) {
       forNew
-        ? postData('/api/sets', form, afterConfirm, setMessage)
-        : putData(`/api/sets/${set._id}`, form, afterConfirm, setMessage)
+        ? postData('/api/sets', form, afterConfirmUpd, setMessage)
+        : putData(`/api/sets/${set._id}`, form, afterConfirmUpd, setMessage)
     } else {
       setErrors({ errs })
       console.log(`errs`, errs)
@@ -329,6 +588,7 @@ export const SetForm = ({
         value={form.description}
         onChange={handleChange}
         required
+        textarea
       />
       <Input
         key="price"
@@ -337,15 +597,6 @@ export const SetForm = ({
         name="price"
         value={form.price / 100}
         onChange={handleChange}
-      />
-      <Input
-        key="image_urls"
-        label="Ссылка на картинку"
-        type="url"
-        name="image_urls"
-        value={form.image_urls[0]}
-        onChange={handleChange}
-        required
       />
       <MultiselectCheckbox
         title="Типы"
@@ -363,6 +614,16 @@ export const SetForm = ({
           })
           // console.log('checked', data)
         }}
+      />
+      <InputImages
+        image_urls={form.image_urls}
+        onChange={(image_urls) =>
+          setForm({
+            ...form,
+            image_urls,
+          })
+        }
+        onAddImage={(image) => sendImage(image, form, setForm)}
       />
     </Form>
   )
