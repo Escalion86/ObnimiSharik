8a65b0331551bbab2a8ba8b88e2838e4e1774034
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-10 10:04:47 +0700

    Fixed Modal style

--- blocks/admin/modals/Modal.js
+++ blocks/admin/modals/Modal.js
@@ -2,8 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Modal = ({ onClose = () => {}, children, onDelete = null }) => (
-  <div className="absolute top-0 left-0 z-30 flex items-center justify-center w-screen h-screen bg-gray-800 bg-opacity-80">
-    <div className="relative">
+  <div className="absolute top-0 left-0 z-30 flex items-center justify-center w-screen h-screen p-5 bg-gray-800 bg-opacity-80">
+    <div className="relative px-5 py-4 my-auto bg-white border-l rounded-xl border-primary w-100">
       <FontAwesomeIcon
         className="absolute w-10 h-10 text-black duration-200 transform cursor-pointer right-4 top-1 hover:scale-110"
         icon={faTimes}
@@ -18,9 +18,7 @@ const Modal = ({ onClose = () => {}, children, onDelete = null }) => (
           onClick={() => onDelete()}
         />
       ) : null}
-      <div className="px-5 py-4 bg-white border-l rounded-xl border-primary w-100">
-        {children}
-      </div>
+      {children}
     </div>
   </div>
 )
