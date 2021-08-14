a8e1d3bd6f46826eca03d9a7f4d3af22f223f105
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-14 14:05:01 +0700

    Fixed Title

--- blocks/admin/Title.js
+++ blocks/admin/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({ text = '', btnOnClick = null, buttons = null }) => {
   return (
-    <div className="sticky top-0 flex items-center justify-center bg-white border-b border-gray-200">
+    <div className="sticky top-0 z-10 flex items-center justify-center bg-white border-b border-gray-200">
       {btnOnClick !== null && btnOnClick !== undefined && (
         <div>
           <a
