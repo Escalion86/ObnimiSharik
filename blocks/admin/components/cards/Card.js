3678f6cb9675ed8b5604661dfbb278075209ce42
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-14 13:49:07 +0700

    Updated ProductCard

--- blocks/admin/components/cards/Card.js
+++ blocks/admin/components/cards/Card.js
@@ -1,5 +1,5 @@
 const Card = ({ children }) => (
-  <div className="flex items-center p-2 mx-1 my-2 bg-white rounded-lg shadow-medium ">
+  <div className="relative flex items-center p-2 mx-1 my-2 bg-white rounded-lg shadow-medium ">
     {children}
   </div>
 )
