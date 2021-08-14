45a435dfaef4022229c589e1fed1be4fadb0455a
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-14 13:49:35 +0700

    Updated ProductCard

--- blocks/admin/content/ProductsContent.js
+++ blocks/admin/content/ProductsContent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ProductCard } from '@admincomponents/cards'
 
 const ProductsContent = ({ data, modals }) => {
-  const { products } = data
+  const { products, productCirculations } = data
 
   return (
     <>
@@ -11,11 +11,22 @@ const ProductsContent = ({ data, modals }) => {
             const types = product.typesId.map((type_id) =>
               data.productTypes.find((typeCheck) => typeCheck._id === type_id)
             )
+            let count = 0
+            productCirculations.forEach((productCirculation) => {
+              if (productCirculation.productId === product._id)
+                if (productCirculation.purchase) {
+                  count -= productCirculation.count
+                } else {
+                  count += productCirculation.count
+                }
+            })
+
             if (types[0] === undefined) types.length = 0
             return (
               <ProductCard
                 key={product._id}
                 product={{ ...product, types }}
+                count={count}
                 onClick={() => modals.openProductModal(product)}
                 onTypeClick={(producttype) =>
                   modals.openProductTypeModal(producttype)
