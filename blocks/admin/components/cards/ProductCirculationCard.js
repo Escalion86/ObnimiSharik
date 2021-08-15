5f74262566b20bef809886869648a445fb1c90b1
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-15 11:27:10 +0700

    Updated ProductCirculationCard

--- blocks/admin/components/cards/ProductCirculationCard.js
+++ blocks/admin/components/cards/ProductCirculationCard.js
@@ -7,7 +7,8 @@ export const ProductCirculationCard = ({
   onClick = () => {},
   onDelete = null,
 }) => {
-  const product = findDataWithId(products, productCirculation?.productId)
+  const { count, purchase, productId } = productCirculation
+  const product = findDataWithId(products, productId)
 
   return (
     <Card>
@@ -21,16 +22,34 @@ export const ProductCirculationCard = ({
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-1/12 text-right">
-        {productCirculation.purchase ? (
-          <div className="flex-1 text-red-700">Продажа</div>
-        ) : (
-          <div className="flex-1 text-green-700">Закуп</div>
-        )}
-        <div className="font-bold">
-          {productCirculation.count ? productCirculation.count : '0'} шт.
-        </div>
+      <div
+        className={
+          'absolute flex justify-center items-center right-0 w-24 h-full text-right rounded-r-lg ' +
+          (purchase ? 'bg-red-200' : 'bg-green-200')
+        }
+      >
+        <span>
+          <span>{count}</span>
+          <span className="text-sm"> шт.</span>
+        </span>
       </div>
+      {/* <div
+      className={
+        'absolute bottom-0 right-0 flex items-center justify-center w-24 h-10 border-t border-l border-gray-300 rounded-tl-lg rounded-br-lg ' +
+        (count > 3
+          ? 'bg-green-400'
+          : count > 0
+          ? 'bg-green-200'
+          : count < 0
+          ? 'bg-red-400'
+          : 'bg-red-200')
+      }
+    >
+      <span>
+        <span>{count}</span>
+        <span className="text-sm"> шт.</span>
+      </span>
+    </div> */}
     </Card>
   )
 }
