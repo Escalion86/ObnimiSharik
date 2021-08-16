a6153ed5b05471167f2cf16b348f72701096c1be
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-16 12:34:39 +0700

    Updated UserCard

--- blocks/admin/components/cards/UserCard.js
+++ blocks/admin/components/cards/UserCard.js
@@ -1,10 +1,13 @@
 import roleRus from '@helpers/roleRus'
 import Card from './Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 export const UserCard = ({ user, onClick = () => {}, onDelete = null }) => {
   return (
     <Card>
-      <div className="flex-1">
+      <div className="items-center flex-1">
         <div className="flex flex-col flex-wrap justify-between gap-x-4 phoneH:flex-row">
           <div
             className="font-semibold cursor-pointer text-primary hover:text-toxic"
@@ -19,6 +22,20 @@ export const UserCard = ({ user, onClick = () => {}, onDelete = null }) => {
         <div className="font-bold">{roleRus(user.role)}</div>
         {/* <div className="italic">{'123'}</div> */}
       </div>
+      {user.phone && (
+        <FontAwesomeIcon
+          className="ml-4 cursor-pointer text-primary hover:text-toxic"
+          icon={faPhone}
+          onClick={() => window.open('tel:+' + user.phone)}
+        />
+      )}
+      {user.whatsapp && (
+        <FontAwesomeIcon
+          className="ml-4 cursor-pointer text-primary hover:text-toxic"
+          icon={faWhatsapp}
+          onClick={() => window.open('https://wa.me/+' + user.phone)}
+        />
+      )}
     </Card>
   )
 }
