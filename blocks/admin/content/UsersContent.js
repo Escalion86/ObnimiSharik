a8618aecb653ff33b65170ede41993e0ad31a113
Aleksei Belinskiy <escalion86@gmail.com>
2021-08-18 12:39:09 +0700

    Updated UsersContent for dev

--- blocks/admin/content/UsersContent.js
+++ blocks/admin/content/UsersContent.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { UserCard } from '@admincomponents/cards'
 
-const UsersContent = ({ data, modals }) => {
+const UsersContent = ({ data, modals, user }) => {
   const { users } = data
   let showedUsers = []
-  if (users) showedUsers = users.filter((user) => user.role !== 'dev')
+  if (users)
+    if (user.role !== 'dev')
+      showedUsers = users.filter((showedUser) => showedUser.role !== 'dev')
+    else showedUsers = users
 
   if (!(showedUsers && showedUsers.length > 0)) return <>'Пользователей нет'</>
 
   return (
     <div>
-      {showedUsers.map((user) => {
+      {showedUsers.map((showedUser) => {
         // const count = sets.filter((set) =>
         //   set.typesId.includes(settype._id)
         // ).length
         return (
           <UserCard
-            key={user._id}
-            user={user}
-            onClick={() => modals.openUserModal(user)}
+            key={showedUser._id}
+            user={showedUser}
+            onClick={() => modals.openUserModal(showedUser)}
           />
         )
       })}
