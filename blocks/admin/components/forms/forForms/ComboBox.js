2582d62d15c2ad0371895a533d5e7f3d4e100154
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-02 02:39:58 +0700

    Fixed ComboBox when defaultValue not exists

--- blocks/admin/components/forms/forForms/ComboBox.js
+++ blocks/admin/components/forms/forForms/ComboBox.js
@@ -7,32 +7,37 @@ const ComboBox = ({
   items,
   required = false,
   inLine = false,
-}) => (
-  <div className={'flex ' + (inLine ? 'flex-row items-center ' : 'flex-col ')}>
-    <label className="w-24" htmlFor={name}>
-      {title}
-    </label>
-    <select
-      name={name}
-      className={
-        'px-2 py-1 bg-gray-200 border rounded-lg ' +
-        (required && !defaultValue ? 'border-red-700' : 'border-gray-700')
-      }
-      onChange={handleChange}
-      defaultValue={defaultValue}
+}) => {
+  const defaultValueExists = !!items.find((item) => item.value === defaultValue)
+  return (
+    <div
+      className={'flex ' + (inLine ? 'flex-row items-center ' : 'flex-col ')}
     >
-      {placeholder && (
-        <option disabled value="">
-          {placeholder}
-        </option>
-      )}
-      {items.map((item, index) => (
-        <option key={'combo' + index} value={item.value}>
-          {item.name}
-        </option>
-      ))}
-    </select>
-  </div>
-)
+      <label className="w-24" htmlFor={name}>
+        {title}
+      </label>
+      <select
+        name={name}
+        className={
+          'px-2 py-1 bg-gray-200 border rounded-lg ' +
+          (required && !defaultValue ? 'border-red-700' : 'border-gray-700')
+        }
+        onChange={handleChange}
+        defaultValue={defaultValueExists ? defaultValue : ''}
+      >
+        {placeholder && (
+          <option disabled value="">
+            {placeholder}
+          </option>
+        )}
+        {items.map((item, index) => (
+          <option key={'combo' + index} value={item.value}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
 
 export default ComboBox
