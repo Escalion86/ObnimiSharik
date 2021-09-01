28056a10f616d598e2987e3cb786c723be6fb31f
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-01 21:55:32 +0700

    Fixed Modal

--- blocks/admin/modals/Modal.js
+++ blocks/admin/modals/Modal.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Modal = ({ children, onClose = () => {}, onDelete = null }) => (
-  <div className="absolute top-0 left-0 z-30 flex items-center justify-center w-screen h-screen p-5 bg-gray-800 bg-opacity-80">
+  <div className="absolute top-0 left-0 z-40 flex items-center justify-center w-screen h-screen p-5 bg-gray-800 bg-opacity-80">
     <div className="relative px-5 py-4 my-auto bg-white border-l rounded-xl border-primary w-100">
       <FontAwesomeIcon
         className="absolute w-10 h-10 text-black duration-200 transform cursor-pointer right-4 top-1 hover:scale-110"
@@ -15,7 +15,10 @@ const Modal = ({ children, onClose = () => {}, onDelete = null }) => (
           className="absolute w-10 h-10 text-red-700 duration-200 transform cursor-pointer top-1 left-4 hover:scale-110"
           icon={faTrash}
           size="1x"
-          onClick={onDelete}
+          onClick={() => {
+            onDelete()
+            onClose()
+          }}
         />
       )}
       {children}
