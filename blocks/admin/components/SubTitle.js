c29a9a6ceef9877b82ae324e30daeeefe83e7394
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-01 21:54:08 +0700

    Added SubTitle component

---
+++ blocks/admin/components/SubTitle.js
@@ -0,0 +1,5 @@
+const SubTitle = ({ title }) => (
+  <div className="my-1 ml-2 text-lg font-semibold">{title}</div>
+)
+
+export default SubTitle
--- blocks/admin/content/SettingsContent.js
+++ blocks/admin/content/SettingsContent.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import IconButton from '@components/IconButton'
 import { faPlus, faDownload } from '@fortawesome/free-solid-svg-icons'
-
-const SubTitle = ({ title }) => (
-  <div className="my-1 ml-2 text-lg font-semibold">{title}</div>
-)
+import SubTitle from '@admincomponents/SubTitle'
 
 const SettingsContent = ({ data, modals }) => {
   return (
