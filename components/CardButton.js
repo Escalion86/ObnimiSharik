c2184927aa616daaf7dc9eafa06cbf8a11a79c6e
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-01 21:58:12 +0700

    Added CardButton

---
+++ components/CardButton.js
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
+
+const CardButton = ({
+  name = '',
+  onClick,
+  className = '',
+  icon = faQuestion,
+  disabled = false,
+}) => (
+  <button
+    onClick={onClick}
+    className={
+      'h-8 w-8 whitespace-nowrap shadow font-futuraDemi flex items-center justify-center hover:bg-toxic' +
+      (className ? ' ' + className : '')
+    }
+    disabled={disabled}
+  >
+    <FontAwesomeIcon className="w-3 h-3 text-white" icon={icon} />
+    {name}
+  </button>
+)
+
+export default CardButton
