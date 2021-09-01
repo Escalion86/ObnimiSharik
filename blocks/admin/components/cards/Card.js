c1b42a28777191036065534648f5fec485fe5241
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-01 21:54:32 +0700

    Updated Card style

--- blocks/admin/components/cards/Card.js
+++ blocks/admin/components/cards/Card.js
@@ -2,7 +2,7 @@ const Card = ({ onClick = null, children }) => (
   <div
     onClick={onClick}
     className={
-      'relative flex items-center p-2 mx-1 my-2 bg-white rounded-lg shadow-medium hover:shadow-medium-active' +
+      'relative flex border border-gray-200 items-center p-2 mx-1 my-2 bg-white rounded-lg shadow-medium hover:shadow-medium-active' +
       (onClick ? ' cursor-pointer' : '')
     }
   >
