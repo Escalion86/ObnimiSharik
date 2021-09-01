f76042692d9621a2cbcf4e376550138c05d912ae
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-02 02:25:13 +0700

    Fixed close SideBar wen click on avatar

--- blocks/admin/Header.js
+++ blocks/admin/Header.js
@@ -14,95 +14,99 @@ const UserMenu = ({
   user = DEFAULT_USER,
   setPageId = () => {},
   onSignOut = () => {},
+  closeMenu = () => {},
 }) => {
   return (
     <div className="z-20 ml-5 -mt-1 -mb-1 h-11 w-11">
       <Menu as="div" className="relative inline-block text-left h-11 w-11">
-        {({ open }) => (
-          <>
-            <div className="absolute left-0 z-50 h-11 w-11">
-              <Menu.Button as="div">
-                <img
-                  className="object-cover rounded-full cursor-pointer h-11 w-11 min-w-9"
-                  src={user.image}
-                  alt="Avatar"
-                />
-              </Menu.Button>
-            </div>
-            <Transition
-              show={open}
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items
-                static
-                className="absolute z-0 w-56 py-1 mt-2 origin-top-right bg-white border divide-y divide-gray-100 border-primary -top-2 right-5 rounded-mdshadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        {({ open }) => {
+          return (
+            <>
+              <div className="absolute left-0 z-50 h-11 w-11">
+                <Menu.Button as="div">
+                  <img
+                    onClick={() => closeMenu()}
+                    className="object-cover rounded-full cursor-pointer h-11 w-11 min-w-9"
+                    src={user.image}
+                    alt="Avatar"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                show={open}
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                <div className="flex items-center pt-1 pl-3 pr-6 text-sm italic font-semibold text-gray-900 min-h-11">
-                  {user.name}
-                </div>
-                <div className="flex items-center pb-1 pl-3 pr-6 text-xs italic font-semibold text-gray-900 min-h-11">
-                  {roleRus(user.role)}
-                </div>
-                <div className="px-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        className={`${
-                          active ? 'bg-primary text-white' : 'text-gray-900'
-                        } group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer`}
-                        onClick={() => setPageId(5)}
-                      >
-                        {active ? (
-                          <FontAwesomeIcon
-                            className="w-5 h-5 mr-2 text-white"
-                            icon={faUser}
-                          />
-                        ) : (
-                          <FontAwesomeIcon
-                            className="w-5 h-5 mr-2 text-primary"
-                            icon={faUser}
-                          />
-                        )}
-                        Параметры учетной записи
-                      </a>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="px-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        className={`${
-                          active ? 'bg-primary text-white' : 'text-gray-900'
-                        } group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer`}
-                        onClick={onSignOut}
-                      >
-                        {active ? (
-                          <FontAwesomeIcon
-                            className="w-5 h-5 mr-2 text-white"
-                            icon={faSignOutAlt}
-                          />
-                        ) : (
-                          <FontAwesomeIcon
-                            className="w-5 h-5 mr-2 text-primary"
-                            icon={faSignOutAlt}
-                          />
-                        )}
-                        Сменить учетную запись
-                      </a>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </Transition>
-          </>
-        )}
+                <Menu.Items
+                  static
+                  className="absolute z-0 w-56 py-1 mt-2 origin-top-right bg-white border divide-y divide-gray-100 border-primary -top-2 right-5 rounded-mdshadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <div className="flex items-center pt-1 pl-3 pr-6 text-sm italic font-semibold text-gray-900 min-h-11">
+                    {user.name}
+                  </div>
+                  <div className="flex items-center pb-1 pl-3 pr-6 text-xs italic font-semibold text-gray-900 min-h-11">
+                    {roleRus(user.role)}
+                  </div>
+                  <div className="px-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${
+                            active ? 'bg-primary text-white' : 'text-gray-900'
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer`}
+                          onClick={() => setPageId(5)}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon
+                              className="w-5 h-5 mr-2 text-white"
+                              icon={faUser}
+                            />
+                          ) : (
+                            <FontAwesomeIcon
+                              className="w-5 h-5 mr-2 text-primary"
+                              icon={faUser}
+                            />
+                          )}
+                          Параметры учетной записи
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${
+                            active ? 'bg-primary text-white' : 'text-gray-900'
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer`}
+                          onClick={onSignOut}
+                        >
+                          {active ? (
+                            <FontAwesomeIcon
+                              className="w-5 h-5 mr-2 text-white"
+                              icon={faSignOutAlt}
+                            />
+                          ) : (
+                            <FontAwesomeIcon
+                              className="w-5 h-5 mr-2 text-primary"
+                              icon={faSignOutAlt}
+                            />
+                          )}
+                          Сменить учетную запись
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </>
+          )
+        }}
       </Menu>
     </div>
   )
@@ -165,7 +169,12 @@ const Header = ({
               </svg>
             </div>
 
-            <UserMenu user={user} setPageId={setPageId} onSignOut={onSignOut} />
+            <UserMenu
+              user={user}
+              setPageId={setPageId}
+              onSignOut={onSignOut}
+              closeMenu={closeMenu}
+            />
           </div>
         </div>
       </header>
