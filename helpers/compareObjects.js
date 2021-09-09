1b582792c32ba301714c1f187237116f16d55d31
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-09 09:25:30 +0700

    Updated compareObjects function

--- helpers/compareObjects.js
+++ helpers/compareObjects.js
@@ -1,13 +1,32 @@
-const compareObjects = (object1, object2) => {
-  for (const key in object1) {
-    // && Array.isArray(object1[key])
-    if (typeof object1[key] === 'object') {
-      if (!compareObjects(object1[key], object2[key])) return false
-    } else {
-      if (object1[key] !== object2[key]) return false
-    }
-  }
-  return true
-}
+// const compareObjects = (object1, object2) => {
+//   console.log(`object1`, object1)
+//   console.log(`object2`, object2)
+
+//   for (const key in object1) {
+//     // && Array.isArray(object1[key])
+//     if (typeof object1[key] === 'object') {
+//       if (Array.isArray(object1[key])) {
+
+//       } else if (!compareObjects(object1[key], object2[key])) return false
+//     } else {
+//       if (object1[key] !== object2[key]) return false
+//     }
+//   }
+//   return true
+// }
+
+// const compareObjects = (a, b) => {
+//   // if (Array.isArray(object1[key]))
+//   if (a.length !== b.length) return false
+//   const uniqueValues = new Set([...a, ...b])
+//   for (const v of uniqueValues) {
+//     const aCount = a.filter((e) => e === v).length
+//     const bCount = b.filter((e) => e === v).length
+//     if (aCount !== bCount) return false
+//   }
+//   return true
+// }
+
+const compareObjects = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 
 export default compareObjects
