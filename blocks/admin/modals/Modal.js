f2349b875c7042b744ff78c6af3031060132818f
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-09 17:30:40 +0700

    Fixed icons in modal

--- blocks/admin/modals/Modal.js
+++ blocks/admin/modals/Modal.js
@@ -5,14 +5,14 @@ const Modal = ({ children, onClose = () => {}, onDelete = null }) => (
   <div className="absolute top-0 left-0 z-40 flex items-center justify-center w-screen h-screen p-5 bg-gray-800 bg-opacity-80">
     <div className="relative px-5 py-4 my-auto bg-white border-l rounded-xl border-primary w-100">
       <FontAwesomeIcon
-        className="absolute w-10 h-10 text-black duration-200 transform cursor-pointer right-4 top-1 hover:scale-110"
+        className="absolute w-6 h-6 text-black duration-200 transform cursor-pointer right-4 top-4 hover:scale-110"
         icon={faTimes}
-        size="lg"
+        size="1x"
         onClick={onClose}
       />
       {onDelete && (
         <FontAwesomeIcon
-          className="absolute w-10 h-10 text-red-700 duration-200 transform cursor-pointer top-1 left-4 hover:scale-110"
+          className="absolute w-5 h-5 text-red-700 duration-200 transform cursor-pointer top-4 left-4 hover:scale-110"
           icon={faTrash}
           size="1x"
           onClick={() => {
