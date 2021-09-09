1bea248845727cefdda359418917f600d546fca1
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-09 09:26:14 +0700

    Added TitleButton component

---
+++ blocks/admin/components/TitleButton.js
@@ -0,0 +1,9 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import IconButton from '@components/IconButton'
+
+const TitleButton = ({ onClick = null, icon = faPlus, active = false }) => {
+  if (!onClick) return null
+  return <IconButton onClick={onClick} inverse icon={icon} active={active} />
+}
+
+export default TitleButton
