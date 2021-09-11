c56e84ad99116d84bf609934fa2cc0930975a7b2
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-11 09:43:40 +0700

    Update IconButton & Fixed Title style

--- blocks/admin/Title.js
+++ blocks/admin/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({ title = '', btnOnClick = null, buttons = null }) => {
   return (
-    <div className="flex items-center justify-center bg-white border-b border-gray-200">
+    <div className="flex items-center justify-center gap-1 bg-white border-b border-gray-200">
       {btnOnClick !== null && btnOnClick !== undefined && (
         <div>
           <a
--- components/IconButton.js
+++ components/IconButton.js
@@ -11,6 +11,7 @@ const IconButton = ({
   icon = faQuestion,
   disabled = false,
   active = false,
+  textPos = 'right',
 }) => (
   <button
     onClick={onClick}
@@ -18,22 +19,17 @@ const IconButton = ({
       (small ? 'text-base h-8' : 'text-lg h-10') +
       (inverse
         ? ' text-white ' + (active ? ' bg-yellow-400' : 'bg-primary')
-        : ' bg-white border-white ' +
-          (active ? ' text-yellow-400' : 'text-primary')) +
-      ' border whitespace-nowrap shadow font-futuraDemi rounded-xl flex items-center justify-center hover:bg-toxic' +
+        : ' bg-white ' + (active ? ' text-yellow-400' : 'text-primary')) +
+      ' gap-2 whitespace-nowrap shadow font-futuraDemi rounded-xl flex items-center justify-center hover:bg-toxic' +
       (animation ? ' animate-pulse-light' : '') +
       (className ? ' ' + className : '') +
-      (name === '' ? (small ? ' w-8' : ' w-10') : ' px-3')
+      (name === '' ? (small ? ' w-8' : ' w-10') : ' px-2.5')
     }
     disabled={disabled}
   >
-    {icon && (
-      <FontAwesomeIcon
-        className={'w-5 h-5 text-white' + (name === '' ? '' : ' mr-2')}
-        icon={icon}
-      />
-    )}
-    {name}
+    {textPos === 'left' ? name : ''}
+    {icon && <FontAwesomeIcon className={'w-5 h-5 text-white'} icon={icon} />}
+    {textPos !== 'left' ? name : ''}
   </button>
 )
 
