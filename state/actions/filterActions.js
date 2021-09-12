1a3f436f3f4a0ccdb3331998533ead3ce2237412
Aleksei Belinskiy <escalion86@gmail.com>
2021-09-13 01:17:10 +0700

    Removed not used reducers and actions

--- state/actions/filterActions.js
+++ state/actions/filterActions.js
@@ -1,11 +1,11 @@
 import {
-  SET_INVITATIONS_FILTER,
-  SET_PRODUCTS_FILTER,
-  SET_PRODUCT_CIRCULSTIONS_FILTER,
-  SET_PRODUCT_TYPES_FILTER,
-  SET_SETS_FILTER,
-  SET_SET_TYPES_FILTER,
-  SET_USERS_FILTER,
+  // SET_INVITATIONS_FILTER,
+  // SET_PRODUCTS_FILTER,
+  // SET_PRODUCT_CIRCULSTIONS_FILTER,
+  // SET_PRODUCT_TYPES_FILTER,
+  // SET_SETS_FILTER,
+  // SET_SET_TYPES_FILTER,
+  // SET_USERS_FILTER,
   SET_FILTER,
 } from 'state/constants'
 
@@ -16,51 +16,51 @@ export const setFilter = (data) => {
   }
 }
 
-export const setProductsFilter = (data) => {
-  return {
-    type: SET_PRODUCTS_FILTER,
-    data,
-  }
-}
+// export const setProductsFilter = (data) => {
+//   return {
+//     type: SET_PRODUCTS_FILTER,
+//     data,
+//   }
+// }
 
-export const setSetsFilter = (data) => {
-  return {
-    type: SET_SETS_FILTER,
-    data,
-  }
-}
+// export const setSetsFilter = (data) => {
+//   return {
+//     type: SET_SETS_FILTER,
+//     data,
+//   }
+// }
 
-export const setProductTypesFilter = (data) => {
-  return {
-    type: SET_PRODUCT_TYPES_FILTER,
-    data,
-  }
-}
+// export const setProductTypesFilter = (data) => {
+//   return {
+//     type: SET_PRODUCT_TYPES_FILTER,
+//     data,
+//   }
+// }
 
-export const setSetTypesFilter = (data) => {
-  return {
-    type: SET_SET_TYPES_FILTER,
-    data,
-  }
-}
+// export const setSetTypesFilter = (data) => {
+//   return {
+//     type: SET_SET_TYPES_FILTER,
+//     data,
+//   }
+// }
 
-export const setProductCirculationsFilter = (data) => {
-  return {
-    type: SET_PRODUCT_CIRCULSTIONS_FILTER,
-    data,
-  }
-}
+// export const setProductCirculationsFilter = (data) => {
+//   return {
+//     type: SET_PRODUCT_CIRCULSTIONS_FILTER,
+//     data,
+//   }
+// }
 
-export const setInvitationsFilter = (data) => {
-  return {
-    type: SET_INVITATIONS_FILTER,
-    data,
-  }
-}
+// export const setInvitationsFilter = (data) => {
+//   return {
+//     type: SET_INVITATIONS_FILTER,
+//     data,
+//   }
+// }
 
-export const setUsersFilter = (data) => {
-  return {
-    type: SET_USERS_FILTER,
-    data,
-  }
-}
+// export const setUsersFilter = (data) => {
+//   return {
+//     type: SET_USERS_FILTER,
+//     data,
+//   }
+// }
--- state/reducers/filterReducer.js
+++ state/reducers/filterReducer.js
@@ -1,11 +1,11 @@
 import {
-  SET_INVITATIONS_FILTER,
-  SET_PRODUCTS_FILTER,
-  SET_PRODUCT_CIRCULSTIONS_FILTER,
-  SET_PRODUCT_TYPES_FILTER,
-  SET_SETS_FILTER,
-  SET_SET_TYPES_FILTER,
-  SET_USERS_FILTER,
+  // SET_INVITATIONS_FILTER,
+  // SET_PRODUCTS_FILTER,
+  // SET_PRODUCT_CIRCULSTIONS_FILTER,
+  // SET_PRODUCT_TYPES_FILTER,
+  // SET_SETS_FILTER,
+  // SET_SET_TYPES_FILTER,
+  // SET_USERS_FILTER,
   SET_FILTER,
 } from 'state/constants'
 
@@ -34,20 +34,20 @@ const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_FILTER:
       return { ...state, ...action.data }
-    case SET_PRODUCTS_FILTER:
-      return { ...state, products: action.data }
-    case SET_SETS_FILTER:
-      return { ...state, sets: action.data }
-    case SET_PRODUCT_CIRCULSTIONS_FILTER:
-      return { ...state, productCirculations: action.data }
-    case SET_PRODUCT_TYPES_FILTER:
-      return { ...state, productTypes: action.data }
-    case SET_SET_TYPES_FILTER:
-      return { ...state, setTypes: action.data }
-    case SET_USERS_FILTER:
-      return { ...state, users: action.data }
-    case SET_INVITATIONS_FILTER:
-      return { ...state, invitations: action.data }
+    // case SET_PRODUCTS_FILTER:
+    //   return { ...state, products: action.data }
+    // case SET_SETS_FILTER:
+    //   return { ...state, sets: action.data }
+    // case SET_PRODUCT_CIRCULSTIONS_FILTER:
+    //   return { ...state, productCirculations: action.data }
+    // case SET_PRODUCT_TYPES_FILTER:
+    //   return { ...state, productTypes: action.data }
+    // case SET_SET_TYPES_FILTER:
+    //   return { ...state, setTypes: action.data }
+    // case SET_USERS_FILTER:
+    //   return { ...state, users: action.data }
+    // case SET_INVITATIONS_FILTER:
+    //   return { ...state, invitations: action.data }
     default:
       return state
   }
