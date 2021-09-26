6a0864a4accb9dbc6b3177247f155bfc77dd3596
Escalion <escalion86@gmail.com>
2021-09-26 23:04:47 +0700

    Added RadioBox component

---
+++ blocks/admin/components/forms/forForms/RadioBox.js
@@ -0,0 +1,36 @@
+const RadioBox = ({
+  checked = false,
+  onClick = null,
+  onChange = null,
+  small = false,
+  label = null,
+  labelPos = 'right',
+  className = null,
+}) => {
+  const Label = () => (
+    <span className="font-normal text-gray-700 dark:text-white">{label}</span>
+  )
+  return (
+    <label
+      className={
+        'flex items-center space-x-2' + (className ? ' ' + className : '')
+      }
+    >
+      {label && labelPos === 'left' && <Label />}
+      <input
+        readOnly
+        checked={checked}
+        type="radio"
+        className={
+          'bg-white border rounded-full border-gray-300 appearance-none form-tick bg-radio checked:bg-primary checked:border-transparent focus:outline-none ' +
+          (small ? 'w-4 h-4' : 'w-5 h-5')
+        }
+        onClick={onClick}
+        onChange={onChange}
+      />
+      {label && labelPos !== 'left' && <Label />}
+    </label>
+  )
+}
+
+export default RadioBox
