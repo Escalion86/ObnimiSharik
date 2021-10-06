b61f0aac50a203902087ea71f70ad13ffcee5c49
Escalion <escalion86@gmail.com>
2021-10-07 02:11:52 +0700

    Updated style FormColumn

--- blocks/admin/components/forms/forForms/FromColumn.js
+++ blocks/admin/components/forms/forForms/FromColumn.js
@@ -1,6 +1,9 @@
 const FormColumn = ({ children, className = null }) => (
   <div
-    className={'flex-1 min-w-76 gap-y-1' + (className ? ' ' + className : '')}
+    className={
+      'flex flex-col flex-1 min-w-76 gap-y-2' +
+      (className ? ' ' + className : '')
+    }
   >
     {children}
   </div>
