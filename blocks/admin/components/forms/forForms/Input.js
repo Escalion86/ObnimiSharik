2ceedb57a9dce869bf6a4445f5f92338e674af9e
Escalion <escalion86@gmail.com>
2021-10-07 02:04:40 +0700

    Updated Input & PriceInput

--- blocks/admin/components/forms/forForms/Input.js
+++ blocks/admin/components/forms/forForms/Input.js
@@ -16,14 +16,47 @@ const Input = ({
   value,
   onChange,
   required = false,
-  textarea,
+  textarea = false,
   accept,
   className,
   disabled = false,
   inLine = false,
-  labelStyle = '',
-  inputStyle = '',
+  labelStyle = null,
+  inputStyle = null,
+  postfix = null,
+  prefix = null,
+  readOnly = false,
+  hidden = false,
 }) => {
+  if (hidden) return null
+  if (readOnly)
+    return (
+      <div
+        className={
+          'flex gap-x-1' +
+          (inLine ? ' flex-row items-center' : ' flex-col') +
+          (className ? ' ' + className : '')
+        }
+      >
+        <label
+          className={
+            'border-b-1 border-primary max-w-min whitespace-nowrap' +
+            (labelStyle ? ' ' + labelStyle : '')
+          }
+          htmlFor={name}
+        >
+          {label}:
+        </label>
+        <div className="flex flex-nowrap gap-x-1">
+          {prefix && <div>{prefix}</div>}
+          <div className="ml-2 italic">
+            {type === 'number' ? parseInt(value) : value}
+          </div>
+          {postfix && <div>{postfix}</div>}
+        </div>
+      </div>
+    )
+
   return (
     <div
       className={
@@ -45,7 +78,59 @@ const Input = ({
         {label}
         {required && <span className="text-red-700">*</span>}
       </label>
-      <InputComponent
+      <div
+        className={
+          'flex flex-1 border rounded-lg flex-nowrap overflow-hidden ' +
+          (required && !value ? 'border-red-700' : 'border-gray-700') +
+          (disabled ? ' bg-gray-300  text-gray-600' : ' bg-gray-200 ') +
+          (inputStyle ? ' ' + inputStyle : '')
+        }
+      >
+        {prefix && (
+          <div className="flex items-center justify-center bg-gray-300 border-r border-gray-700 rounded-l-lg min-w-6">
+            {prefix}
+          </div>
+        )}
+        <div className="flex-1 overflow-hidden">
+          <InputComponent
+            className="flex-grow-0 w-full max-w-full px-2 py-1 bg-gray-200 outline-none"
+            type={type === 'number' ? 'text' : type}
+            maxLength={maxLength}
+            name={name}
+            value={type === 'number' ? parseInt(value) : value}
+            onChange={
+              type === 'number'
+                ? (e) => {
+                    e.target.value = Number(e.target.value)
+                    onChange(e)
+                  }
+                : onChange
+            }
+            required={required}
+            textarea={textarea}
+            accept={accept}
+            disabled={disabled}
+            onKeyPress={
+              type === 'number'
+                ? (e) => {
+                    e = e || window.event
+                    var charCode =
+                      typeof e.which == 'undefined' ? e.keyCode : e.which
+                    if (!(charCode >= 48 && charCode <= 57)) {
+                      e?.preventDefault()
+                    }
+                  }
+                : null
+            }
+          />
+        </div>
+        {postfix && (
+          <div className="flex items-center justify-center w-6 bg-gray-300 border-l border-gray-700 rounded-r-lg">
+            {postfix}
+          </div>
+        )}
+      </div>
+      {/* <InputComponent
         className={
           'flex-1 px-2 py-1 border rounded-lg outline-none' +
           (required && !value ? ' border-red-700' : ' border-gray-700') +
@@ -80,7 +165,7 @@ const Input = ({
               }
             : null
         }
-      />
+      /> */}
     </div>
   )
 }
--- blocks/admin/components/forms/forForms/PriceInput.js
+++ blocks/admin/components/forms/forForms/PriceInput.js
@@ -1,61 +1,30 @@
+import Input from './Input'
+
 const PriceInput = ({
   value,
-  title = 'Стоимость',
+  label = 'Стоимость',
   onChange,
   required = false,
   className,
   inLine,
   labelStyle,
   name = 'price',
+  readOnly = false,
 }) => {
-  if (!parseInt(value)) value = 0
   return (
-    <div
-      className={
-        'flex' +
-        (inLine ? ' flex-row items-center' : ' flex-col') +
-        (className ? ' ' + className : '')
-      }
-    >
-      <label
-        className={
-          labelStyle
-            ? ' ' + labelStyle
-            : inLine
-            ? 'min-w-24 max-w-40 w-1/4'
-            : ''
-        }
-        htmlFor={name}
-      >
-        {title}
-        {required && <span className="text-red-700">*</span>}
-      </label>
-      <div
-        className={
-          'flex w-full border rounded-lg flex-nowrap max-w-30 ' +
-          (required && !value ? 'border-red-700' : 'border-gray-700')
-        }
-      >
-        <input
-          className="flex-1 w-24 px-2 py-1 bg-gray-200 rounded-l-lg outline-none"
-          type="text"
-          name={name}
-          value={parseInt(value)}
-          onChange={onChange}
-          required={required}
-          onKeyPress={(e) => {
-            e = e || window.event
-            var charCode = typeof e.which == 'undefined' ? e.keyCode : e.which
-            if (!(charCode >= 48 && charCode <= 57)) {
-              e?.preventDefault()
-            }
-          }}
-        />
-        <div className="flex items-center justify-center w-6 bg-gray-300 border-l border-gray-700 rounded-r-lg">
-          ₽
-        </div>
-      </div>
-    </div>
+    <Input
+      label={label}
+      className={'max-w-40' + (className ? ' ' + className : '')}
+      labelStyle={labelStyle}
+      type="number"
+      name={name}
+      value={value}
+      onChange={onChange}
+      required={required}
+      inLine={inLine}
+      postfix="₽"
+      readOnly={readOnly}
+    />
   )
 }
 
