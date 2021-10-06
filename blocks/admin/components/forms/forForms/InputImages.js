aa40a50a491e1bc45f3add3072bf6cfd2aa7296d
Escalion <escalion86@gmail.com>
2021-10-07 02:12:34 +0700

    Added readOnly prop to InputImages

--- blocks/admin/components/forms/forForms/InputImages.js
+++ blocks/admin/components/forms/forForms/InputImages.js
@@ -9,6 +9,7 @@ const InputImages = ({
   onChange = () => {},
   onAddImage = () => {},
   required = false,
+  readOnly = false,
 }) => {
   const hiddenFileInput = useRef(null)
   const addImageClick = (event) => {
@@ -21,32 +22,51 @@ const InputImages = ({
 
   return (
     <div className="flex flex-col">
-      <label>
-        Картинки{required && <span className="text-red-700">*</span>}
+      <label
+        className={
+          readOnly
+            ? 'border-b-1 border-primary max-w-min whitespace-nowrap'
+            : ''
+        }
+      >
+        Картинки
+        {readOnly ? ':' : required && <span className="text-red-700">*</span>}
       </label>
       <div
         className={
-          'flex flex-wrap w-full gap-2 px-1.5 py-1 bg-gray-200 border rounded-lg ' +
-          (required && !images?.length ? 'border-red-700' : 'border-gray-700')
+          'flex flex-wrap w-full gap-2 px-1.5 py-1 rounded-lg ' +
+          (readOnly
+            ? ''
+            : ' bg-gray-200 border' +
+              (required && !images?.length
+                ? 'border-red-700'
+                : 'border-gray-700'))
         }
       >
         {images.map((image, index) => (
-          <div key={index} className="relative">
+          <div
+            key={index}
+            className={
+              'relative h-20 overflow-hidden' +
+              (readOnly ? ' border border-gray-400 hover:border-primary' : '')
+            }
+          >
             <Zoom zoomMargin={20}>
               <img className="w-20" src={image} alt="item_image" />
             </Zoom>
-            <FontAwesomeIcon
-              className="absolute top-0 right-0 text-red-700 duration-200 transform cursor-pointer hover:scale-125"
-              icon={faTrash}
-              size="1x"
-              onClick={() => {
-                // images.splice(index, 1)
-                onChange(images.filter((image, i) => i !== index))
-              }}
-            />
+            {!readOnly && (
+              <FontAwesomeIcon
+                className="absolute top-0 right-0 text-red-700 duration-200 transform cursor-pointer hover:scale-125"
+                icon={faTrash}
+                size="1x"
+                onClick={() => {
+                  onChange(images.filter((image, i) => i !== index))
+                }}
+              />
+            )}
           </div>
         ))}
-        {images.length < 4 && (
+        {images.length < 4 && !readOnly && (
           <div
             onClick={addImageClick}
             className="flex items-center justify-center w-20 h-20 bg-white border-2 border-gray-500 cursor-pointer rounded-xl"
