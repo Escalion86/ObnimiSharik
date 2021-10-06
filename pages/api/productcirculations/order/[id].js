ce8c54c73866c7c1b660e4e0fd4d8e3465dff209
Escalion <escalion86@gmail.com>
2021-10-07 02:16:55 +0700

    Added api\productcirculations\order

---
+++ pages/api/productcirculations/order/[id].js
@@ -0,0 +1,7 @@
+// import dbConnect from '@utils/dbConnect'
+import ProductCirculations from '@models/ProductCirculations'
+import CRUD from '@server/CRUD'
+
+export default async function handler(req, res) {
+  return await CRUD(ProductCirculations, req, res, 'orderId')
+}
