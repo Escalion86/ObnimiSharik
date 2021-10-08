6741d7c75e93ad2c4b74538ab33208f68e79689c
Escalion <escalion86@gmail.com>
2021-10-08 14:31:19 +0700

    Updated formatDeliveryAddress helper

--- helpers/formatDeliveryAddress.js
+++ helpers/formatDeliveryAddress.js
@@ -1,9 +1,15 @@
 const formatDeliveryAddress = (deliveryAddress) => {
   if (!deliveryAddress?.town && !deliveryAddress?.street) return 'неизвестен'
-  return `${deliveryAddress?.town && deliveryAddress.town + ', '}${
-    deliveryAddress?.street &&
-    deliveryAddress?.flat &&
-    deliveryAddress.street + ' - ' + deliveryAddress.flat
+  return `${
+    deliveryAddress?.town &&
+    deliveryAddress.town + (deliveryAddress?.street ? ', ' : '')
+  }${
+    (deliveryAddress?.street && deliveryAddress?.house
+      ? deliveryAddress.street + deliveryAddress.house
+      : '') +
+    (deliveryAddress.house && deliveryAddress.flat
+      ? ' - ' + deliveryAddress.flat
+      : '')
   }`
 }
 
