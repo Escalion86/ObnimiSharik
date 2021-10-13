fa5b4873c509adfec50e5f517d0d41820328aa3b
Escalion <escalion86@gmail.com>
2021-10-13 17:47:56 +0700

    Updated PaymentCard

--- blocks/admin/components/cards/PaymentCard.js
+++ blocks/admin/components/cards/PaymentCard.js
@@ -1,6 +1,8 @@
 import Card from './Card'
 import CardButtons from './forCards/CardButtons'
 import CardContainer from './CardContainer'
+import formatDateTime from '@helpers/formatDateTime'
+import { useSelector } from 'react-redux'
 
 export const PaymentCard = ({
   payment,
@@ -8,18 +10,27 @@ export const PaymentCard = ({
   onEdit = null,
   onDelete = null,
 }) => {
+  const { orders } = useSelector((state) => state)
+  const order = orders.find((order) => order._id === payment.orderId)
   return (
     <Card inLine onClick={() => onClick(payment)}>
-      <CardContainer>
+      <CardContainer className="justify-between">
         <div className="items-center flex-1">
           <div className="flex flex-col flex-wrap justify-between gap-x-4 phoneH:flex-row">
-            <div className="font-semibold">id: {payment._id}</div>
+            <div>
+              <span className="font-semibold">№ {payment.number}</span> от{' '}
+              <span className="font-semibold">
+                {formatDateTime(payment.payAt)}
+              </span>
+            </div>
           </div>
         </div>
-        {/* <div className="text-right"> */}
-        {/* <div className="font-bold">{roleRus(user.role)}</div> */}
-        {/* <div className="italic">{'123'}</div> */}
-        {/* </div> */}
+        <div className="flex-1 italic">
+          Заказ № {order.number} на {formatDateTime(order.deliveryDateFrom)}
+        </div>
+        <div className="text-right">
+          <div className="font-bold">{payment.sum} ₽</div>
+        </div>
       </CardContainer>
       <CardButtons onEdit={onEdit} onDelete={onDelete} />
     </Card>
