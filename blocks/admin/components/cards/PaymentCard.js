99fa7cf00329ff397089c80e68a482836cbb8904
Escalion <escalion86@gmail.com>
2021-10-17 23:41:35 +0700

    Updated PaymentCard

--- blocks/admin/components/cards/PaymentCard.js
+++ blocks/admin/components/cards/PaymentCard.js
@@ -3,6 +3,9 @@ import CardButtons from './forCards/CardButtons'
 import CardContainer from './CardContainer'
 import formatDateTime from '@helpers/formatDateTime'
 import { useSelector } from 'react-redux'
+import { PAY_TYPES } from '@helpers/constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 
 export const PaymentCard = ({
   payment,
@@ -12,8 +15,19 @@ export const PaymentCard = ({
 }) => {
   const { orders } = useSelector((state) => state)
   const order = orders.find((order) => order._id === payment.orderId)
+  const payType = PAY_TYPES.find(
+    (payTypeItem) => payTypeItem.value === payment.payType
+  )
   return (
     <Card inLine onClick={() => onClick(payment)}>
+      <div
+        className={
+          'flex justify-center items-center w-9 rounded-l-lg text-white bg-' +
+          (payType ? payType.color + '-400' : 'gray-400')
+        }
+      >
+        <FontAwesomeIcon icon={payType?.icon ?? faQuestion} size="lg" />
+      </div>
       <CardContainer className="justify-between">
         <div className="items-center flex-1">
           <div className="flex flex-col flex-wrap justify-between gap-x-4 phoneH:flex-row">
