30f599575c2c417d40fa9a40157d00d9f602468b
Escalion <escalion86@gmail.com>
2021-10-17 23:41:02 +0700

    Added PayTypePicker

---
+++ blocks/admin/components/forms/forForms/PropValuePicker/PayTypePicker.js
@@ -0,0 +1,27 @@
+import { PAY_TYPES } from '@helpers/constants'
+import PropValuePicker from './PropValuePicker'
+
+const PayTypePicker = ({
+  payType,
+  onChange = null,
+  inLine = false,
+  className = null,
+  labelStyle = null,
+  required = false,
+  readOnly = false,
+}) => (
+  <PropValuePicker
+    value={payType}
+    valuesArray={PAY_TYPES}
+    label="Тип"
+    onChange={onChange}
+    inLine={inLine}
+    className={className}
+    labelStyle={labelStyle}
+    name="payType"
+    required={required}
+    readOnly={readOnly}
+  />
+)
+
+export default PayTypePicker
