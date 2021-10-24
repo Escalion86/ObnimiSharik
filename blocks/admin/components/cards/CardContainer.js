199bf9588c09d5de810d077fd2e4cb7102add239
Escalion <escalion86@gmail.com>
2021-10-25 00:37:18 +0700

    Updated CardContainer style

--- blocks/admin/components/cards/CardContainer.js
+++ blocks/admin/components/cards/CardContainer.js
@@ -1,5 +1,5 @@
 const CardContainer = ({ children, className = null }) => (
-  <div className={'flex flex-1 p-2' + (className ? ' ' + className : '')}>
+  <div className={'flex flex-1 p-2 gap-2' + (className ? ' ' + className : '')}>
     {children}
   </div>
 )
