a055b32de45a1641780779752e1af2f8d9652345
Escalion <escalion86@gmail.com>
2021-10-25 23:27:15 +0700

    Updated TypeCard (added image)

--- blocks/admin/components/cards/TypeCard.js
+++ blocks/admin/components/cards/TypeCard.js
@@ -15,26 +15,46 @@ export const TypeCard = ({
   onAdd = null,
   onDelete = null,
   onEdit = null,
-}) => (
-  <Card inLine onClick={() => onClick(type)}>
-    <CardContainer>
-      <div className="flex-1">
-        <div className="flex justify-between gap-x-2">
-          <div className="font-semibold">{type.name}</div>
-          {/* <div className="flex-1 italic">{type.description}</div> */}
+}) => {
+  const imageClassName = 'w-24'
+  return (
+    <Card onClick={() => onClick(type)}>
+      <CardContainer>
+        {type.image ? (
+          <div onClick={(event) => event.stopPropagation()}>
+            <Zoom zoomMargin={20}>
+              <img className={imageClassName} src={type.image} alt="product" />
+            </Zoom>
+          </div>
+        ) : (
+          <img
+            className={imageClassName}
+            src="/img/no_image.png"
+            alt="product"
+          />
+        )}
+        <div className="flex-1">
+          <div className="flex flex-col justify-between gap-x-2 tablet:flex-row">
+            <div className="w-5/12 font-semibold min-w-40">{type.name}</div>
+            <div className="flex-1 text-sm italic min-w-40">
+              {type.description}
+            </div>
+          </div>
+        </div>
+      </CardContainer>
+      <div className="flex flex-col items-end justify-between">
+        <CardButtons
+          onAdd={onAdd}
+          onEdit={onEdit}
+          onDelete={onDelete}
+          topRight
+        />
+        <div className="px-1 font-bold text-right whitespace-nowrap min-w-min">
+          {type.count} шт.
         </div>
       </div>
-      <div className="w-20 text-right">
-        <span>
-          <span>{type.count}</span>
-          <span className="text-sm"> шт.</span>
-        </span>
-        {/* <div className="font-bold">{count !== null ? count : '0'} шт.</div> */}
-        {/* <div className="">{products.price} ₽</div> */}
-      </div>
-    </CardContainer>
-    <CardButtons onAdd={onAdd} onEdit={onEdit} onDelete={onDelete} stretch />
-  </Card>
-)
+    </Card>
+  )
+}
 
 export default TypeCard
