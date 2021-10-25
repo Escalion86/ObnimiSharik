3aa6288f16c7a5f798ad46cc1049b38a8972e0bd
Escalion <escalion86@gmail.com>
2021-10-25 23:30:39 +0700

    Updated model for ProductType & SetType

--- models/SetTypes.js
+++ models/SetTypes.js
@@ -7,6 +7,19 @@ const SetTypesSchema = new mongoose.Schema({
     maxlength: [100, 'Название типа набора не может превышать 100 смволов'],
     default: '',
   },
+  description: {
+    type: String,
+    maxlength: [600, 'Описание не может превышать 600 символов'],
+    default: '',
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  cardSize: {
+    type: String,
+    default: 'normal', // big, small
+  },
   createdAt: {
     type: Date,
     default: Date.now,
