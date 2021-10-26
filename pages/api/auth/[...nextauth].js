0f078020bc917d46b9fb0a9d67506f5359010aff
Escalion <escalion86@gmail.com>
2021-10-26 09:17:58 +0700

    Added authorizationUrl to Google provider nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -28,7 +28,7 @@ export default NextAuth({
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+      authorizationUrl: process.env.GOOGLE_AUTHORIZATION_URL,
     }),
     // ...add more providers here
   ],
