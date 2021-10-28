dafbcac8e5ee0a530bc09f9511fc575f3b551114
Escalion <escalion86@gmail.com>
2021-10-28 23:38:24 +0700

    test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -28,7 +28,8 @@ export default NextAuth({
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl: process.env.GOOGLE_AUTHORIZATION_URL,
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
     // ...add more providers here
   ],
