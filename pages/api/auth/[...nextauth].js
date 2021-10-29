57021dc99392535605f05951294550632ea971cc
Escalion <escalion86@gmail.com>
2021-10-29 13:13:48 +0700

    Remove mongodb from nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,38 +6,38 @@ import Invitations from '@models/Invitations'
 import CRUD from '@server/CRUD'
 // import Auth0Provider from 'next-auth/providers/auth0'
 import GoogleProvider from 'next-auth/providers/google'
-import VkProvider from 'next-auth/providers/vk'
+// import VkProvider from 'next-auth/providers/vk'
 // import EmailProvider from 'next-auth/providers/email'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+// import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 
-import { MongoClient } from 'mongodb'
+// import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-}
+// const uri = process.env.MONGODB_URI
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// }
 
-let client
-let clientPromise
+// let client
+// let clientPromise
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Please add your Mongo URI to .env.local')
+// }
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   // In development mode, use a global variable so that the value
+//   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options)
+//     global._mongoClientPromise = client.connect()
+//   }
+//   clientPromise = global._mongoClientPromise
+// } else {
+//   // In production mode, it's best to not use a global variable.
+//   client = new MongoClient(uri, options)
+//   clientPromise = client.connect()
+// }
 
 const defaultUserProps = {
   role: 'client',
@@ -65,10 +65,10 @@ export default NextAuth({
       // authorizationUrl:
       //   'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
-    VkProvider({
-      clientId: process.env.VK_CLIENT_ID,
-      clientSecret: process.env.VK_CLIENT_SECRET,
-    }),
+    // VkProvider({
+    //   clientId: process.env.VK_CLIENT_ID,
+    //   clientSecret: process.env.VK_CLIENT_SECRET,
+    // }),
     // ...add more providers here
   ],
   callbacks: {
@@ -179,7 +179,7 @@ export default NextAuth({
   // A database is optional, but required to persist accounts in a database
   // database: process.env.MONGODB_URI,
   // adapter: TypeORMLegacyAdapter(process.env.MONGODB_URI)
-  adapter: MongoDBAdapter({
-    db: (await clientPromise).db('obnimisharik'),
-  }),
+  // adapter: MongoDBAdapter({
+  //   db: (await clientPromise).db('obnimisharik'),
+  // }),
 })
