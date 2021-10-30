3450b6bce24acea5f020f28755b17ec994af710a
Escalion <escalion86@gmail.com>
2021-10-30 21:06:49 +0700

    Test

--- blocks/admin/content/TestContent.js
+++ blocks/admin/content/TestContent.js
@@ -1,7 +1,24 @@
+import Button from '@components/Button'
+import emailSend from '@helpers/emailSend'
 import React from 'react'
 
 const TestContent = ({ data, modals, loggedUser }) => {
-  return <div className="px-3"></div>
+  console.log(`window.location.href`, window.location.href)
+  return (
+    <div className="px-3">
+      <Button
+        onClick={() => {
+          emailSend({
+            To: '2562020@list.ru',
+            Subject: 'Test',
+            Body: 'BODY',
+          })
+        }}
+        inverse
+        name="Отправка письма"
+      />
+    </div>
+  )
 }
 
 export default TestContent
