7f784e398d5034e753365282b915dc3524dea5c0
Escalion <escalion86@gmail.com>
2021-11-03 10:52:20 +0700

    Updated roleRus result text

--- helpers/roleRus.js
+++ helpers/roleRus.js
@@ -1,7 +1,7 @@
 import { ROLES } from './constants'
 
 const roleRus = (role) => {
-  return ROLES.find((roleRes) => roleRes.value === role)?.name ?? 'Клиент'
+  return ROLES.find((roleRes) => roleRes.value === role)?.name ?? '[без роли]'
 }
 
 export default roleRus
