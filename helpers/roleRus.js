801ab8bcb86e213bcd50d934df3e8ccb0f031fb6
Escalion <escalion86@gmail.com>
2021-11-03 10:42:24 +0700

    Fixed error in roleRus when unexpected role

--- helpers/roleRus.js
+++ helpers/roleRus.js
@@ -1,7 +1,7 @@
 import { ROLES } from './constants'
 
 const roleRus = (role) => {
-  return ROLES.find((roleRes) => roleRes.value === role).name
+  return ROLES.find((roleRes) => roleRes.value === role)?.name ?? 'Клиент'
 }
 
 export default roleRus
