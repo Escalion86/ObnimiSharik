8d6c499dd94f379f640bc365685d9db67690c6f3
Escalion <escalion86@gmail.com>
2021-11-03 09:58:19 +0700

    test

--- blocks/admin/components/cards/UserCard.js
+++ blocks/admin/components/cards/UserCard.js
@@ -14,27 +14,26 @@ export const UserCard = ({
 }) => {
   // const gender = GENDERS.find((genderItem) => genderItem.value === user.gender)
   return (
-    <div></div>
-    // <Card className="items-center" inLine onClick={() => onClick(user)}>
-    //   <ZoomImage
-    //     image={user.image}
-    //     noImage={`/img/users/${user.gender ?? 'noGender'}.jpg`}
-    //     alt="user"
-    //     imageClassName="h-full w-24 max-h-24 laptop:max-h-10 laptop:w-10"
-    //     containerClassName="h-full rounded-l-lg"
-    //   />
-    //   <CardContainer className="items-center">
-    //     <div className="flex flex-col justify-between gap-x-4 laptop:flex-row">
-    //       <div className="font-semibold">{user.name}</div>
-    //       <div className="italic">{user.email}</div>
-    //       <div className="flex-1 font-bold">{roleRus(user.role)}</div>
-    //     </div>
-    //   </CardContainer>
-    //   <div className="flex flex-col-reverse items-end justify-between laptop:flex-row laptop:items-center">
-    //     <ContactsIconsButtons user={user} />
-    //     <CardButtons onEdit={onEdit} onDelete={onDelete} />
-    //   </div>
-    // </Card>
+    <Card className="items-center" inLine onClick={() => onClick(user)}>
+      <ZoomImage
+        image={user?.image}
+        noImage={`/img/users/${user.gender ?? 'noGender'}.jpg`}
+        alt="user"
+        imageClassName="h-full w-24 max-h-24 laptop:max-h-10 laptop:w-10"
+        containerClassName="h-full rounded-l-lg"
+      />
+      <CardContainer className="items-center">
+        <div className="flex flex-col justify-between gap-x-4 laptop:flex-row">
+          <div className="font-semibold">{user?.name ?? ''}</div>
+          <div className="italic">{user?.email ?? ''}</div>
+          <div className="flex-1 font-bold">{roleRus(user?.role ?? '')}</div>
+        </div>
+      </CardContainer>
+      <div className="flex flex-col-reverse items-end justify-between laptop:flex-row laptop:items-center">
+        <ContactsIconsButtons user={user} />
+        <CardButtons onEdit={onEdit} onDelete={onDelete} />
+      </div>
+    </Card>
   )
 }
 
