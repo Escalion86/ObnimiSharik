c1dccccabdcd925f7cf6bca3f33834a117149fb1
Escalion <escalion86@gmail.com>
2021-11-03 10:41:54 +0700

    test finished

--- blocks/admin/content/UsersContent.js
+++ blocks/admin/content/UsersContent.js
@@ -3,40 +3,31 @@ import { UserCard } from '@admincomponents/cards'
 import { Virtuoso } from 'react-virtuoso'
 
 const UsersContent = ({ data, modals, loggedUser }) => {
-  console.log(`data[1]`, data[1])
-  console.log(`data[2]`, data[2])
-  try {
-    const users = data
-    let showedUsers = []
-    if (users)
-      if (loggedUser.role !== 'dev')
-        showedUsers = users.filter((showedUser) => showedUser.role !== 'dev')
-      else showedUsers = users
+  const users = data
+  let showedUsers = []
+  if (users)
+    if (loggedUser.role !== 'dev')
+      showedUsers = users.filter((showedUser) => showedUser.role !== 'dev')
+    else showedUsers = users
 
-    if (!(showedUsers && showedUsers.length > 0))
-      return <div className="px-3">'Пользователей нет'</div>
+  if (!(showedUsers && showedUsers.length > 0))
+    return <div className="px-3">'Пользователей нет'</div>
 
-    return (
-      <Virtuoso
-        data={showedUsers}
-        itemContent={(index, showedUser) => {
-          if (index === 2) return <div>-</div>
-          return (
-            <UserCard
-              loggedUser={loggedUser}
-              key={showedUser._id}
-              user={showedUser}
-              onClick={() => modals.openUserModal(showedUser)}
-              onEdit={() => modals.openUserModal(showedUser, null, null, true)}
-              onDelete={() => modals.openDeleteUser(showedUser)}
-            />
-          )
-        }}
-      />
-    )
-  } catch (e) {
-    console.log(`error`, e)
-  }
+  return (
+    <Virtuoso
+      data={showedUsers}
+      itemContent={(index, showedUser) => (
+        <UserCard
+          loggedUser={loggedUser}
+          key={showedUser._id}
+          user={showedUser}
+          onClick={() => modals.openUserModal(showedUser)}
+          onEdit={() => modals.openUserModal(showedUser, null, null, true)}
+          onDelete={() => modals.openDeleteUser(showedUser)}
+        />
+      )}
+    />
+  )
 }
 
 export default UsersContent
