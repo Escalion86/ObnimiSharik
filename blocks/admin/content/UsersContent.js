863430c61ea5b475229f823d6d3a907569f296d9
Escalion <escalion86@gmail.com>
2021-11-03 10:11:21 +0700

    test

--- blocks/admin/content/UsersContent.js
+++ blocks/admin/content/UsersContent.js
@@ -3,31 +3,38 @@ import { UserCard } from '@admincomponents/cards'
 import { Virtuoso } from 'react-virtuoso'
 
 const UsersContent = ({ data, modals, loggedUser }) => {
-  const users = data
-  let showedUsers = []
-  if (users)
-    if (loggedUser.role !== 'dev')
-      showedUsers = users.filter((showedUser) => showedUser.role !== 'dev')
-    else showedUsers = users
+  console.log(`data`, data)
+  console.log(`modals`, modals)
+  console.log(`loggedUser`, loggedUser)
+  try {
+    const users = data
+    let showedUsers = []
+    if (users)
+      if (loggedUser.role !== 'dev')
+        showedUsers = users.filter((showedUser) => showedUser.role !== 'dev')
+      else showedUsers = users
 
-  if (!(showedUsers && showedUsers.length > 0))
-    return <div className="px-3">'Пользователей нет'</div>
+    if (!(showedUsers && showedUsers.length > 0))
+      return <div className="px-3">'Пользователей нет'</div>
 
-  return (
-    <Virtuoso
-      data={showedUsers}
-      itemContent={(index, showedUser) => (
-        <UserCard
-          loggedUser={loggedUser}
-          key={showedUser._id}
-          user={showedUser}
-          onClick={() => modals.openUserModal(showedUser)}
-          onEdit={() => modals.openUserModal(showedUser, null, null, true)}
-          onDelete={() => modals.openDeleteUser(showedUser)}
-        />
-      )}
-    />
-  )
+    return (
+      <Virtuoso
+        data={showedUsers}
+        itemContent={(index, showedUser) => (
+          <UserCard
+            loggedUser={loggedUser}
+            key={showedUser._id}
+            user={showedUser}
+            onClick={() => modals.openUserModal(showedUser)}
+            onEdit={() => modals.openUserModal(showedUser, null, null, true)}
+            onDelete={() => modals.openDeleteUser(showedUser)}
+          />
+        )}
+      />
+    )
+  } catch (e) {
+    console.log(`error`, e)
+  }
 }
 
 export default UsersContent
