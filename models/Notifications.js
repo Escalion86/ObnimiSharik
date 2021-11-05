b09f25d72e64657f4a4fec63272d0b335f87237d
Escalion <escalion86@gmail.com>
2021-11-05 18:42:49 +0700

    Updated NotificationsSchema

--- models/Notifications.js
+++ models/Notifications.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose'
 
 const NotificationsSchema = new mongoose.Schema(
   {
-    title: { type: String },
+    responsibleUserId: { type: String, default: '' },
+    title: { type: String, default: '' },
     description: { type: String, default: '' },
-    dbName: { type: String },
-    itemId: { type: String },
-    status: { type: String },
+    dbName: { type: String, default: '' },
+    itemId: { type: String, default: '' },
+    oldItem: { type: mongoose.Schema.Types.Mixed, default: null },
+    newItem: { type: mongoose.Schema.Types.Mixed, default: null },
+    status: { type: String, default: null },
   },
   { timestamps: true }
 )
