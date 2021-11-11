f9d47444a6a9c9cc8f68eb35c21b3c8cf4f1922a
Escalion <escalion86@gmail.com>
2021-11-11 22:08:06 +0700

    Added notifications reducer and action

--- state/actions/notificationsActions.js
+++ state/actions/notificationsActions.js
@@ -1,8 +1,15 @@
-import { SET_NOTIFICATIONS } from 'state/constants'
+import { SET_NOTIFICATIONS, SET_NOTIFICATION_VIEWED } from 'state/constants'
 
 export const setNotifications = (notifications) => {
   return {
     type: SET_NOTIFICATIONS,
     notifications,
+  }
+}
+
+export const setNotificationViewed = (id) => {
+  return {
+    type: SET_NOTIFICATION_VIEWED,
+    id,
   }
 }
--- state/reducers/notificationsReducer.js
+++ state/reducers/notificationsReducer.js
@@ -1,9 +1,14 @@
-import { SET_NOTIFICATIONS } from 'state/constants'
+import { SET_NOTIFICATIONS, SET_NOTIFICATION_VIEWED } from 'state/constants'
 
 const notificationsReducer = (state = [], action) => {
   switch (action.type) {
     case SET_NOTIFICATIONS:
       return action.notifications ?? state
+    case SET_NOTIFICATION_VIEWED:
+      return state.map((note) => {
+        if (note._id === action.id) return { ...note, viewed: true }
+        return note
+      })
     default:
       return state
   }
