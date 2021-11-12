6e905156098d71dd9a8a8fb96a36b39634bcb69c
Escalion <escalion86@gmail.com>
2021-11-12 13:25:37 +0700

    Fixed admin api for notifications

--- pages/api/admin/index.js
+++ pages/api/admin/index.js
@@ -50,7 +50,7 @@ export default async function handler(req, res) {
 
         const notifications = await Notifications.find({
           _id: { $nin: arrayOfIdsSeenUserNotifications },
-          // responsibleUserId: { $ne: user._id },
+          responsibleUserId: { $ne: user._id },
         })
         res.status(200).json({
           success: true,
