9908f6015f97e809e84efab6d9abee55b0fbf66e
Escalion <escalion86@gmail.com>
2021-11-21 00:38:05 +0700

    Updated IconButton

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,5 +1,5 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 
 const IconButton = ({
   name = '',
@@ -8,28 +8,53 @@ const IconButton = ({
   animation = false,
   small = false,
   inverse = false,
-  icon = faQuestion,
+  icon = null,
   disabled = false,
   active = false,
   textPos = 'right',
+  loading = false,
 }) => (
   <button
     onClick={onClick}
     className={
+      'flex justify-center items-center duration-300 ' +
+      (textPos = 'right' ? 'flex-row-reverse ' : '') +
+      (disabled || loading ? 'cursor-not-allowed ' : '') +
       (small ? 'text-base h-8' : 'text-lg h-10') +
       (inverse
-        ? ' text-white ' + (active ? ' bg-yellow-400' : 'bg-primary')
-        : ' bg-white ' + (active ? ' text-yellow-400' : 'text-primary')) +
-      ' gap-2 whitespace-nowrap shadow font-futuraDemi rounded-xl flex items-center justify-center hover:bg-toxic' +
+        ? disabled || loading
+          ? ' bg-gray-400 text-white'
+          : ' text-white hover:bg-toxic hover:border-toxic hover:text-white' +
+            (active ? ' bg-yellow-400' : ' bg-primary')
+        : ' border' +
+          (disabled || loading
+            ? ' bg-white text-gray-400 border-gray-400'
+            : ' bg-white hover:bg-toxic hover:border-toxic  hover:text-white' +
+              (active
+                ? ' text-yellow-400 border-yellow-400'
+                : ' text-primary border-primary'))) +
+      ' gap-2 whitespace-nowrap shadow font-futuraDemi rounded-xl flex items-center justify-center' +
       (animation ? ' animate-pulse-light' : '') +
       (className ? ' ' + className : '') +
       (name === '' ? (small ? ' w-8' : ' w-10') : ' px-2.5')
     }
-    disabled={disabled}
+    disabled={disabled || loading}
   >
-    {textPos === 'left' ? name : ''}
-    {icon && <FontAwesomeIcon className={'w-5 h-5 text-white'} icon={icon} />}
-    {textPos !== 'left' ? name : ''}
+    {name && (
+      <div
+        className={
+          'duration-300 max-w-100 overflow-hidden' + (loading ? ' max-w-0' : '')
+        }
+      >
+        {name}
+      </div>
+    )}
+    {(icon || loading) && (
+      <FontAwesomeIcon
+        className={'w-5 h-5' + (loading ? ' fa-spin' : '')}
+        icon={loading ? faSpinner : icon}
+      />
+    )}
   </button>
 )
 
